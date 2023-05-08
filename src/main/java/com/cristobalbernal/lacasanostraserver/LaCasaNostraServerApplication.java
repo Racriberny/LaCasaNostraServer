bba19c812f7b74039cf3023e5a7c0df624d59426
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-05-08 22:30:13 +0200

    adios promociones!!!

--- src/main/java/com/cristobalbernal/lacasanostraserver/LaCasaNostraServerApplication.java
+++ src/main/java/com/cristobalbernal/lacasanostraserver/LaCasaNostraServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LaCasaNostraServerApplication {
     public static boolean DEBUG = true;
     public static String CONFIG_FILE =  null;
-
     public static void main(String[] args) {
         SpringApplication.run(LaCasaNostraServerApplication.class, args);
     }
--- src/main/java/com/cristobalbernal/lacasanostraserver/repo/IPromocionesDao.java
+++
@@ -1,7 +0,0 @@
-package com.cristobalbernal.lacasanostraserver.repo;
-
-import com.cristobalbernal.lacasanostraserver.entidades.Promociones;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPromocionesDao extends JpaRepository<Promociones,Integer>{
-}
